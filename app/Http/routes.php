dfc3e369c2471e078781116bad255703f78cf607
Haris Subandie Md Suhaimin <haris@vms.my>
2016-01-30 01:25:35 +0800

    completing basic tutorial. example not working.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -30,21 +30,41 @@ Route::group(['middleware' => ['web']], function () {
     /**
      * Show Task Dashboard
      */
-    Route::get('/', function () {
-        //
-    });
+Route::get('/', function () {
+    $tasks = Task::orderBy('created_at', 'asc')->get();
+
+    return view('tasks', [
+        'tasks' => $tasks
+    ]);
+});
 
     /**
      * Add New Task
      */
     Route::post('/task', function (Request $request) {
-        //
-    });
+	    $validator = Validator::make($request->all(), [
+	        'name' => 'required|max:255',
+	    ]);
+
+	    if ($validator->fails()) {
+	        return redirect('/')
+	            ->withInput()
+	            ->withErrors($validator);
+	    }
+
+	    $task = new Task;
+	    $task->name = $request->name;
+	    $task->save();
+
+	    return redirect('/');
+		});
 
     /**
      * Delete Task
      */
     Route::delete('/task/{task}', function (Task $task) {
-        //
+        $task->delete();
+
+    	return redirect('/');
     });
 });
