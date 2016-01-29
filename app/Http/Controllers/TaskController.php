bf7585e4187e4b53da5ce4463ff250074a478255
Haris Subandie Md Suhaimin <haris@vms.my>
2016-01-30 02:02:23 +0800

    snapshot

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -17,5 +17,51 @@ class TaskController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+        $this->tasks = $tasks;
     }
+
+    /**
+	 * Display a list of all of the user's task.
+	 *
+	 * @param  Request  $request
+	 * @return Response
+	 */
+	public function index(Request $request)
+	{
+	    return view('tasks.index', [
+            'tasks' => $this->tasks->forUser($request->user()),
+        ]);
+	}
+
+	/**
+	 * Create a new task.
+	 *
+	 * @param  Request  $request
+	 * @return Response
+	 */
+	public function store(Request $request)
+	{
+	    $this->validate($request, [
+	        'name' => 'required|max:255',
+	    ]);
+
+	    // Create The Task...
+	    $request->user()->tasks()->create([
+	        'name' => $request->name,
+	    ]);
+
+	    return redirect('/tasks');
+	}
+
+	/**
+	 * Destroy the given task.
+	 *
+	 * @param  Request  $request
+	 * @param  Task  $task
+	 * @return Response
+	 */
+	public function destroy(Request $request, Task $task)
+	{
+	    //
+	}
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -33,3 +33,4 @@ Route::group(['middleware' => ['web']], function () {
 Route::get('/tasks', 'TaskController@index');
 Route::post('/task', 'TaskController@store');
 Route::delete('/task/{task}', 'TaskController@destroy');
+
