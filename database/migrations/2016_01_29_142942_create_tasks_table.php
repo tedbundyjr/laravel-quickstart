88f5745a74bbb6b2037f34cf1a13e2191917eca7
Haris Subandie Md Suhaimin <haris@vms.my>
2016-01-29 22:31:53 +0800

    $table->string('name');

--- database/migrations/2016_01_29_142942_create_tasks_table.php
+++ database/migrations/2016_01_29_142942_create_tasks_table.php
@@ -14,6 +14,7 @@ class CreateTasksTable extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name');
             $table->timestamps();
         });
     }
