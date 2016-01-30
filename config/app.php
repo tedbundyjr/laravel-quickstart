3d80f6e259cd7ebc47de936e0a843650fc19b288
Haris Subandie Md Suhaimin <haris@vms.my>
2016-01-30 16:25:31 +0800

    composer require barryvdh/laravel-debugbar

--- config/app.php
+++ config/app.php
@@ -26,7 +26,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -202,7 +202,7 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
-
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
     ],
 
 ];
